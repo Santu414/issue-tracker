fc00a63a9ae3d9f6a7ee6e43627861c6772e3d53
Santu414 <snagankera@gmail.com>
2023-12-20 03:10:01 +0530

    Loading page conditionally

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,16 +1,33 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
 const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  const currentPath = usePathname();
   return (
     <nav className="flex space-x-6 border-b md-5 px-5 h-14 items-center">
-      <Link href="/">Logo</Link>
+      <Link href="/">
+        <AiFillBug />
+      </Link>
       <ul className="flex space-x-6">
-        <li>
-          <Link href={"/"}>Dashboard</Link>
-        </li>
-        <li>
-          <Link href={"/issues"}>Issues</Link>
-        </li>
+        {links.map((link) => (
+          <li>
+            <Link
+              key={link.href}
+              className={`${
+                link.href === currentPath ? "text-zinc-500" : "text-zinc-800"
+              } hover: transition-colors`}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
